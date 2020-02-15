03cb9b6fafdd3ab77fc8283a626ebef1e26c86e0
Kunal Vyas <kunalapune@gmail.com>
2020-02-15 23:28:22 +0530

    Unit Converter

---
+++ SpringBoot/UnitConverter/Unit-Converter/src/main/java/com/kunal/spring/unit_converter/Unit/Converter/UnitConverterApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.spring.unit_converter.Unit.Converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitConverterApplication.class, args);
+	}
+
+}
